4d08d9b3d2c09d2f770d6e2b76a7a811d2455f56
Harish Dhanraj Sugandhi <68777106+harishsganndhi123@users.noreply.github.com>
2024-01-16 11:05:12 +0530

    database connected

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import dotenv from "dotenv";
 import app from "./app.js";
-
+import connectDB from "./db/dbConnection.js";
 
 dotenv.config({
   path: "./env",
 });
 
-const port = process.env.PORT || 8000;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port} 🔥`);
-});
+connectDB()
+  .then(() => {
+    try {
+      const port = process.env.PORT;
+      app.listen(port, () => {
+        console.log(`Server running on port ${port} 🔥`);
+      });
+    } catch (err) {
+      console.log("Error in Creating Server connection !! ", err);
+    }
+  })
+  .catch((err) => {
+    console.log("Error in DB connection !! ", err);
+  });
